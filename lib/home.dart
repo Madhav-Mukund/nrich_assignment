af53860c590ff051917fbf96ad7e74442971ebd5
Madhav-Mukund <madkaus@gmail.com>
2023-04-24 18:32:55 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+class ImageUploadScreen extends StatefulWidget {
+  @override
+  _ImageUploadScreenState createState() => _ImageUploadScreenState();
+}
+
+class _ImageUploadScreenState extends State<ImageUploadScreen> {
+  File? _image;
+
+  Future<void> _getImageFromGallery() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future<void> _uploadImage() async {
+    if (_image == null) {
+      return;
+    }
+
+    var request = http.MultipartRequest(
+        'POST', Uri.parse('https://codelime.in/api/remind-app-token'));
+
+    request.files.add(await http.MultipartFile.fromPath('image', _image!.path));
+
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('The image has been uploaded successfully.'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Failed to upload image'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Image Upload')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _image == null
+                ? const Text('No image selected')
+                : Image.file(_image!, width: 300, height: 300),
+            ElevatedButton(
+              onPressed: _getImageFromGallery,
+              child: const Text('Select Image'),
+            ),
+            ElevatedButton(
+              onPressed: _uploadImage,
+              child: const Text('Upload Image'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
