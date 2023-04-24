5ec1731616549981c3f9f4e5f945bdcb38236792
Madhav-Mukund <madkaus@gmail.com>
2023-04-24 20:31:46 +0530

    Final changes

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,6 +11,8 @@ class ImageUploadScreen extends StatefulWidget {
 
 class _ImageUploadScreenState extends State<ImageUploadScreen> {
   File? _image;
+  final blue = const Color.fromARGB(255, 26, 35, 126);
+  final yellow = const Color.fromARGB(255, 255, 193, 7);
 
   Future<void> _getImageFromGallery() async {
     final pickedFile =
@@ -53,25 +56,73 @@ class _ImageUploadScreenState extends State<ImageUploadScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Image Upload')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _image == null
-                ? Text('No image selected')
-                : Image.file(_image!, width: 300, height: 300),
-            ElevatedButton(
-              onPressed: _getImageFromGallery,
-              child: Text('Select Image'),
-            ),
-            ElevatedButton(
-              onPressed: _uploadImage,
-              child: Text('Upload Image'),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(
+            'Upload Image',
+            style: GoogleFonts.lato(),
+          ),
+          backgroundColor: blue,
+          shape: Border(
+            bottom: BorderSide(color: yellow, width: 3),
+          ),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color.fromARGB(255, 72, 72, 72),
+            ),
+            height: 560,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _image == null
+                    ? Row()
+                    : Row(
+                        children: [
+                          const Spacer(),
+                          IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _image = null;
+                                });
+                              },
+                              icon: const Icon(Icons.cancel_outlined))
+                        ],
+                      ),
+                _image == null
+                    ? Text(
+                        'No image selected',
+                        style: GoogleFonts.lato(fontSize: 16),
+                      )
+                    : Image.file(_image!, width: 360, height: 360),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: _getImageFromGallery,
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(blue)),
+                  child: Text(
+                    'Select Image',
+                    style: GoogleFonts.lato(
+                        color: yellow, fontWeight: FontWeight.w600),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _uploadImage,
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(blue)),
+                  child: Text(
+                    'Upload Image',
+                    style: GoogleFonts.lato(
+                        color: yellow, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
